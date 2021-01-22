edeb0ecb0d923c92975f5b923d5b62a452e8ab7d
murat gültekin <muratgultekin2000@hotmail.com>
2021-01-22 22:07:32 +0300

    Add project files.

---
+++ MyDictionary/MyDictionaryList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionaryList<number,name>
+    {
+        number[] _keys;
+        name[] _values;
+        public MyDictionaryList()
+        {
+            _keys = new number[0];
+            _values = new name[0];
+        }
+        public void Add(number key,name value)
+        {
+            number[] tempKeys = _keys;
+            name[] tempValue = _values;
+            _keys = new number[_keys.Length + 1];
+            _values = new name[_values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+                _values[i] = tempValue[i];
+            }
+
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get { return _keys.Length; }
+        }
+        public number[] Keys
+        {
+            get { return _keys; }
+        }
+        public name[] Values
+        {
+            get { return _values; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionaryList<string, string> myDictionaryList = new MyDictionaryList<string, string>();
+            myDictionaryList.Add("5055055252", "murat gül");
+            myDictionaryList.Add("5055054747", "ali can");
+            myDictionaryList.Add("5055053745", "mustafa kafa");
+
+            for (int i = 0; i < myDictionaryList.Length; i++)
+            {
+                Console.WriteLine(myDictionaryList.Keys[i]+":"+myDictionaryList.Values[i]);
+            }
+        }
+    }
+}
